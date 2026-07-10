f8317f28e0a9913e6543f58ea678f862cc5fe37a
ChapaBandara <it23152496@my.sliit.lk>
2026-07-10 10:28:24 +0530

    Add NexaBuild logo to Navbar and favicon

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import Home from '../pages/Home'
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
