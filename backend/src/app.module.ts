dc4d6ac11188b8255878ee6f71a165ef6c3f5543
Ganindu <ganindujayasundara11@gmail.com>
2026-08-04 12:21:00 +0530

    feat(backend): align land schema with frontend, apply migrations and setup controller/service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PropertyModule } from './property/property.module';
+import { LandModule } from './land/land.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PropertyModule, LandModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
