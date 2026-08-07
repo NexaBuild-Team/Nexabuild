cca0d651456df5846e55ec7f2e56b7ff211e73a4
NuhaMana <nuhamanajirr@gmail.com>
2026-08-07 14:47:18 +0530

    feat: complete architecture backend and cloud database seed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PropertyModule } from './property/property.module';
+import { ArchitectureModule } from './architecture/architecture.module';
 
 
 
 @Module({
-  imports: [PrismaModule, PropertyModule],
+  imports: [PrismaModule, PropertyModule, ArchitectureModule],
   controllers: [AppController],
   providers: [AppService],
 })
